a7338640e853cf5ed810794a9ad8c515bbbf1c7b
ShreyashSiddharth <shreyashsiddharth@gmail.com>
2023-07-09 21:21:50 +0530

    Final Commit

--- script.js
+++ script.js
@@ -43,3 +43,16 @@ getWeather('Seattle');
 
 
 
+
+const getWeatherDefault = (city) =>{
+
+	
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		
+		console.log(response)
+	})
+	.catch(err => console.error(err));
+};

a24acda126e9eda0746b9b9045fb84c29d7735f4
ShreyashSiddharth <shreyashsiddharth@gmail.com>
2023-07-09 20:27:17 +0530

    Harrys Code complete

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 Weather
 
 Tells weather about any city using Rapid API
+
+cloud_pct = response.cloud_
+feels_like = response.feels_like
+humidity = response.humidity
+max_temp = response.max_temp
+min_temp = response.min_temp
+sunrise = response.sunrise
+sunset = response.sunset
+Temp = response.temp
+Wind_Degree = response.wind_degrees
+Wind_Speed = response.wind_speed
+
+
+cloud_pct
+temp
+feels_like
+humidity
+min_temp
+max_temp
+wind_speed
+wind_degrees
+sunrise
+sunset
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,9 +7,39 @@ const options = {
 	}
 };
 
-fetch(url,options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+const getWeather = (city) =>{
 
+	cityName.innerHTML = city;
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+
+		tinfo.innerHTML = response.temp
+		hinfo.innerHTML = response.humidity
+		winfo.innerHTML = response.wind_speed
+
+
+		
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		max_temp.innerHTML = response.max_temp
+		min_temp.innerHTML = response.min_temp
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+		temp.innerHTML = response.temp
+		wind_degree.innerHTML = response.wind_degrees
+		wind_speed.innerHTML = response.wind_speed
+		console.log(response)
+	})
+	.catch(err => console.error(err));
+};
+
+submit.addEventListener("click",(e)=>{
+	e.preventDefault();
+	getWeather(city.value)
+})
     //Watch at 21:57
+getWeather('Seattle');
+
+
+
